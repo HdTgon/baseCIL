af1067723800cb55f2f510bbcf58211945a8878e
HdTgon <64786070+HdTgon@users.noreply.github.com>
2026-04-04 13:52:12 +0800

    Delete auto directory

--- auto/statistics_classifier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-datasets=("aircraft" "cars" "cifar" "cub" "food" "imagenetr" "ucf" "sun" "aircraftb50" "carsb50" "cifarb50" "cubb100" "foodb50" "imagenetrb100" "ucfb50" "sunb150")
-
-for dataset in "${datasets[@]}"; do
-    config_file="./exps/visual_classifier/${dataset}.json"
-        echo "Running with dataset=${dataset}, visual_classifier"
-        CUDA_VISIBLE_DEVICES=7 python main.py --config "$config_file" --num_sampled 256
-done
--- auto/statistics_gmm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-datasets=("aircraft" "cars" "cifar" "cub" "food" "sun")
-
-for dataset in "${datasets[@]}"; do
-    config_file="./exps/gmm/${dataset}.json"
-        echo "Running with dataset=${dataset}, gmm"
-        CUDA_VISIBLE_DEVICES=7 python main.py --config "$config_file"
-done
